284a2ea4e138c837db9319fdf25b6b15f71e1b95
0Tick <96893104+0Tick@users.noreply.github.com>
2025-11-18 19:25:53 +0100

    Add nix lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
+vim.lsp.config["nil"] = {
+  cmd = { "nil" },
+  filetypes = { "nix" },
+  root_markers = { "flake.nix", "home.nix" }
+}
+
+
 local servers = { "html", "cssls", "nil" }
 vim.lsp.enable(servers)
 
